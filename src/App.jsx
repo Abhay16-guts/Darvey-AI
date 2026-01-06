684822f13445bdcf3e5f45ebc72a154956c09d48
Abhay Singh Parihar <abhayparihar23aug@gmail.com>
2026-01-06 16:26:51 +0530

    google gen ai integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import { useState } from 'react';
+import { GoogleGenAI } from "@google/genai";
 import styles from './App.module.css';
 import { Chat } from './components/Chat/chat';
 import { Controls } from './components/Chat/Controls/Controls';
 
+const googleai = new GoogleGenAI(import.meta.env.VITE_GEMINI_API_KEY);
+const gemini = googleai.getGenerativeModel({model: "gemini-1.5-flash",});
+const chat = gemini.startChat({history: [],});
+
 function App() {
   const [messages, setMessages] = useState([]);
 
-function handleContentSend(content){
-  setMessages((prevMessages)=>[...prevMessages, { content, role:"user"}])
-}
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content, role: "user" });
+    try {
+      const result = await chat.sendMessage(content);
+      addMessage({ content: result.response.text(), role: "assistant" });
+    } catch (error) {
+      addMessage({ content: "Sorry ! I couldn't process your request. Please try again !", role: "system" });
+    }
+  }
 
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
-        <img src="/chatbot.png" className={styles.Logo}/>
+        <img src="/chatbot.png" className={styles.Logo} />
         <h2 className={styles.Title}>Darvey AI</h2>
       </header>
       <div className={styles.ChatContainer}>
         <Chat messages={messages} />
       </div>
-      <Controls onSend={handleContentSend}/>
+      <Controls onSend={handleContentSend} />
     </div>
   );
 }
